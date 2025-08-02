5b29a6f5bad1c1f33f6cfe7a2a448cad1f1873f8
pushpakumari98 <pushpa20052002kumari@gmail.com>
2025-08-02 07:53:56 +0530

    updated

--- Apple.java
+++ Apple.java
@@ -1,4 +1,10 @@
+import java.awt.Rectangle;
 
 public class Apple {
+
+    public boolean intersects(Rectangle bird) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'intersects'");
+    }
 
 }
